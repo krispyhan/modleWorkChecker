116fdfd9e69c731031e6a98437fca24f4602db52
krispyhan <krispyhan@163.com>
2021-03-29 16:43:52 +0800

    ReserveController加上@RestController

--- src/main/java/com/hsy/mybatis/controller/ReserveController.java
+++ src/main/java/com/hsy/mybatis/controller/ReserveController.java
@@ -1,21 +1,19 @@
 package com.hsy.mybatis.controller;
 
 
-import com.hsy.mybatis.mapper.IReserveServiceMapper;
 import com.hsy.mybatis.service.IReserveService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
 
+@RestController
 public class ReserveController {
 
     @Autowired
     private IReserveService reserveService;
-
-    @Autowired
-    private IReserveServiceMapper reserveServiceMapper;
 
     @RequestMapping("/reserve_{theme}_{contact}_{time}_{content}")
     public String sendReservation(@PathVariable String theme,@PathVariable String contact,
